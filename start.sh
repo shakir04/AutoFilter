8948de0b51b191cf3177adb9ac3025ced248f4d3
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 15:32:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/shakir04/AutoFilter.git /Lucy
+  git clone https://github.com/shakir04/AutoFilter.git /AutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lucy
+  git clone $UPSTREAM_REPO /AutoFilter
 fi
 cd /Lucy
 pip3 install -U -r requirements.txt
