73532d6214704761ecbb28ff304f72e2113a6f2e
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 17:59:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -94,7 +94,7 @@ REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1 week')
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'mr_shakir_04') # owner username without @
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001745077023')) #Log channel id ( make sure bot is admin )
-DUMP_CHNL = int(environ.get('DUMP_CHNL', ''))
+DUMP_CHNL = int(environ.get('DUMP_CHNL', '-1001745077023'))
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
