70d922703585924b4d82e66359735c0a90b50437
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 16:37:02 +0530

    Update song.py

--- plugins/modules/song.py
+++ plugins/modules/song.py
@@ -51,7 +51,7 @@ async def song(_, message):
         await k.edit("ᴜᴘʟᴏᴀᴅɪɴɢ")
         song_title = info.get("title", "Unknown Title")   
         song_caption = f"**🍃 {song_title}**\n" + \
-                       f"🍂 sᴜᴘᴘᴏʀᴛ: <a href='https://t.me/weebs_support'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>" 
+                       f"🍂 sᴜᴘᴘᴏʀᴛ: <a href='https://t.me/paid_method_zone_discussion'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>" 
 
         await message.reply_audio(
             path,
