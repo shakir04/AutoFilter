cf702dc8ff39e7216c3ec19d1dde3a68fa31e652
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 18:35:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,8 +70,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://shakirkhan:shakirkhan01@king.kihedre.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "king")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://modderking:king01@cluster0.n49ksqz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
