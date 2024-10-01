d5553da8104f2b6ebe5977611cb03a6bc7a26a71
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 16:36:22 +0530

    Update settitle.py

--- plugins/modules/settitle.py
+++ plugins/modules/settitle.py
@@ -48,7 +48,7 @@ async def who_is(bot, message):
 #    protect = "/pbatch" if PROTECT_CONTENT else "batch"
 #     mention = message.from_user.mention
 #     buttons = [[
-#         InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/movie7xchat')
+#         InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/paid_method_zone_discussion')
 #     ]]
 #    reply_markup = InlineKeyboardMarkup(buttons)
 #     await message.reply_text(START_MESSAGE.format(message.from_user.mention, message.chat.title),
