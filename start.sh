42654780d2638f8481dc3b844b8dc54b9ce05c9c
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 14:18:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/shakir04/Lucy.git /Lucy
+  git clone https://github.com/shakir04/Lucy-main.git /Lucy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucy
