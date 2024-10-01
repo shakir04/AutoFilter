721922a0aef7a15a3ea953a24fbd3f736d1e3f7e
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-01 16:42:30 +0530

    Update google.py

--- plugins/Extra/google.py
+++ plugins/Extra/google.py
@@ -122,7 +122,7 @@ async def search_(Client: Client, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @iam_daxx")
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @mr_shakir_04")
         print(f"error : {e}")
         return
 
@@ -191,6 +191,6 @@ async def stack_search_(Client: Client, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @DevsOops")
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @mr_shakir_04")
         print(f"error : {e}")
         return
