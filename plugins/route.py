dd98b1ba2dcde7a80b046940f9b452b07dcc2a2e
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-03 10:17:37 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("LucyAutoFilter")
+    return web.json_response("moviesottkingbot")
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
