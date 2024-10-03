f385c6409a0b293d8acea005454b1aa05d35a042
Modder King <127608272+shakir04@users.noreply.github.com>
2024-10-03 10:20:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilter
 fi
-cd /Lucy
+cd /AutoFilter
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ ʟᴜᴄʏ ʙᴏᴛ...."
 python3 bot.py
